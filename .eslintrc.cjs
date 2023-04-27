2189aefab193bad135b9a884e52e8f114d2d813f
nATOr <gowluonade@gmail.com>
2023-04-27 15:41:11 -0500

    pnpm add ts-standard -D

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json'
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
