a62eceb88e77af61153146dfe7ef302d8dc57fe9
nATOr <gowluonade@gmail.com>
2023-04-27 17:03:08 -0500

    txt_code-syntax-highlighter

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,18 @@
 import { Card, Typography } from '@mui/material'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import { useQuestionsStore } from './store/questions'
 import { type Question as QuestionType } from './types.d'
+
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant='outlined'>
+    <Card variant='outlined' sx={{ textAlign: 'left' }}>
       <Typography variant='h5'>
         {info.question}
       </Typography>
+      <SyntaxHighlighter language='javascript' style={gradientDark}>
+        {info.code}
+      </SyntaxHighlighter>
     </Card>
   )
 }
