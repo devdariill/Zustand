3b87648bbf3fdea0494eddabc49e61d7497f6467
nATOr <gowluonade@gmail.com>
2023-04-27 16:13:13 -0500

    read_store

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Container, Stack, Typography } from '@mui/material'
 import './App.css'
 import { Start } from './Start'
 import { JavaScriptLogo } from './components/JavaScript'
+import { useQuestionsStore } from './store/questions'
 
 function App () {
+  const questions = useQuestionsStore(state => state.questions)
+  console.log(questions)
   return (
     <main>
       <Container maxWidth='sm'>
