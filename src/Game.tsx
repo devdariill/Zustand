38e8327d68f70bc09be8fcb19943fe40812b9cf2
nATOr <gowluonade@gmail.com>
2023-04-27 17:12:04 -0500

    component-MaterialUI

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography } from '@mui/material'
+import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import { useQuestionsStore } from './store/questions'
@@ -6,13 +6,22 @@ import { type Question as QuestionType } from './types.d'
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant='outlined' sx={{ textAlign: 'left' }}>
+    <Card variant='outlined' sx={{ textAlign: 'left', p: 2, marginTop: 4 }}>
       <Typography variant='h5'>
         {info.question}
       </Typography>
       <SyntaxHighlighter language='javascript' style={gradientDark}>
         {info.code}
       </SyntaxHighlighter>
+      <List sx={{ bgcolor: '#333' }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton>
+              <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   )
 }
