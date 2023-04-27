0968944adbdd66e926febde36a66e2600590cded
nATOr <gowluonade@gmail.com>
2023-04-27 16:22:07 -0500

    _

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
   }
 }
