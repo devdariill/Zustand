e83e147acb2f506b634a8d0a4e74023553d5e0fc
nATOr <gowluonade@gmail.com>
2023-05-02 17:30:14 -0500

    extension redux devtools chrome

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,7 +1,8 @@
 import confetti from 'canvas-confetti'
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import { type Question } from '../types.d'
+// extension redux devtools chrome
 
 interface State {
   questions: Question[]
@@ -27,7 +28,7 @@ const logger = (config) => (set: any, get: any, api: any) => {
   )
 }
 
-export const useQuestionsStore = create<State>()(logger(persist((set, get) => {
+export const useQuestionsStore = create<State>()(devtools(logger(persist((set, get) => {
   return {
     questions: [],
     currentQuestion: 0,
@@ -36,7 +37,7 @@ export const useQuestionsStore = create<State>()(logger(persist((set, get) => {
       const res = await fetch('http://localhost:5173/data.json')
       const data = await res.json()
       const questions = data.sort(() => Math.random() - 0.5).slice(0, limit)
-      set({ questions })
+      set({ questions }, false, 'FETCH_QUESTIONS')
     },
     selectAnswer: (questionId: number, answerIndex: number) => {
       const { questions } = get()
@@ -53,20 +54,20 @@ export const useQuestionsStore = create<State>()(logger(persist((set, get) => {
         isCorrectUserAnswer,
         userSelectedAnswer: answerIndex
       }
-      set({ questions: newQuestions })
+      set({ questions: newQuestions }, false, 'SELECT_ANSWER')
     },
     goToNextQuestion: () => {
       const { currentQuestion, questions } = get()
       const nextQuestion = currentQuestion + 1
       if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion })
+        set({ currentQuestion: nextQuestion }, false, 'GO_TO_NEXT_QUESTION')
       }
     },
     goToPreviousQuestion: () => {
       const { currentQuestion } = get()
       const previousQuestion = currentQuestion - 1
       if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion })
+        set({ currentQuestion: previousQuestion }, false, 'GO_TO_PREVIOUS_QUESTION')
       }
     },
     reset: () => {
@@ -77,4 +78,4 @@ export const useQuestionsStore = create<State>()(logger(persist((set, get) => {
   name: 'questions'
   // getStorage: localStorage // default
   // getStorage: sessionStorage
-})))
+}))))
